c0ef40e58f8b64836341ba3240d9e75e1064f98d
Max Tyson <63089527+maxtyson123@users.noreply.github.com>
2023-05-04 21:49:21 +1200

    Add analytics

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import type { AppProps } from 'next/app'
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { Analytics } from '@vercel/analytics/react';
 
 // Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <>
         <main>
           <Component {...pageProps} />
+          <Analytics />
         </main>
       </>
   )
