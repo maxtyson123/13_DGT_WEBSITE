9b6049c890eb39a9e20e9d4928741df4d5d55840
Max Tyson <98maxt98@gmail.com>
2023-06-08 22:04:24 +1200

    Begin Theming

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,6 +1,5 @@
 //set PATH=%PATH%;C:\Users\max.tyson\Downloads\node-v14.16.0-win-x64\node-v14.16.0-win-x64
 
-// TODO: Responsive navbar
 // TODO: Accessibility Settings (Dark mode)
 // TODO: Redo the images for the plants
 // TODO: Use SQL
