76c985173073de6e480cd1291925f3880c3cc61b
max.tyson <max.tyson@wbhs.school.nz>
2024-02-09 13:30:20 +1300

    Begin Working on admin page

---
+++ website/src/pages/admin/index.tsx
@@ -0,0 +1,158 @@
+import HtmlHeader from "@/components/html_header";
+import Navbar from "@/components/navbar";
+import Section from "@/components/section";
+import PageHeader from "@/components/page_header";
+import styles from "@/styles/pages/admin.module.css";
+import Link from "next/link";
+import {useEffect, useRef, useState} from "react";
+import Footer from "@/components/footer";
+import {loginSection} from "@/pages/account";
+import {useSession} from "next-auth/react";
+import {Loading} from "@/components/loading";
+import {Error} from "@/components/error";
+import {checkUserPermissions, RongoaUser} from "@/lib/users";
+import {makeCachedRequest} from "@/lib/api_tools";
+export default function Admin(){
+    const pageName = "admin";
+
+
+    const { data: session } = useSession()
+
+    // Stats
+    const [numberOfPlants, setNumberOfPlants] = useState(0)
+    const [numberOfUsers, setNumberOfUsers] = useState(0)
+    const [numberOfSettings, setNumberOfSettings] = useState(0)
+
+    // Load the data
+    const [loading, setLoading] = useState(true)
+    const [loadingMessage, setLoadingMessage] = useState("Loading...")
+    const [error, setError] = useState("")
+
+
+    // Don't fetch the data again if it has already been fetched
+    const dataFetch = useRef(false)
+    useEffect(() => {
+        // Fetch the data
+        if (dataFetch.current)
+            return
+
+        dataFetch.current = true
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+
+        // Check the user permissions
+        if(!checkUserPermissions(session?.user as RongoaUser, "pages:admin:publicAccess")){
+            setError("You do not have permission to access this page.")
+            setLoading(false)
+            return
+        }
+
+        // Fetch the user count
+        setLoadingMessage("Fetching user count...")
+
+        // Fetch the plant count
+        setLoadingMessage("Fetching plant count...")
+        const user = await makeCachedRequest("plant_stats", "/api/plants/uses")
+        if(!user){
+            setError("Plant data not found")
+            setLoading(false)
+            return
+        }
+        setNumberOfPlants(user.data.length)
+
+        // Fetch the settings count
+        setLoadingMessage("Fetching settings count...")
+
+        // Finish loading
+        setLoading(false)
+    }
+
+    const adminPage = () => {
+        return (
+            <>
+
+                {/* Section for the welcome message and search box */}
+                <Section>
+                    <div className={styles.adminHeaderContainer}>
+                        <h1>Welcome to the Admin Page</h1>
+                        <p>Here you can manage the website content and settings.</p>
+
+                        <Link href={"/admin/plants"}>
+                            <button>Manage Plants</button>
+                        </Link>
+                        <Link href={"/admin/users"}>
+                            <button>Manage Users</button>
+                        </Link>
+                        <Link href={"/admin/settings"}>
+                            <button>Manage Settings</button>
+                        </Link>
+                    </div>
+
+                    <div className={styles.adminDataContainer}>
+                        <div className={styles.adminData}>
+                            <h2>Plants</h2>
+                            <p>There are currently 0 plants in the database.</p>
+                        </div>
+                        <div className={styles.adminData}>
+                            <h2>Users</h2>
+                            <p>There are currently 0 users in the database.</p>
+                        </div>
+                        <div className={styles.adminData}>
+                            <h2>Settings</h2>
+                            <p>There are currently 0 settings in the database.</p>
+                        </div>
+                    </div>
+                </Section>
+
+            </>
+        )
+    }
+
+    return(
+        <>
+
+            {/* Set up the page header and navbar */}
+            <HtmlHeader currentPage={pageName}/>
+            <Navbar currentPage={pageName}/>
+
+
+            {/* Header for the page */}
+            <Section>
+                <PageHeader size={"medium"}>
+                    <h1>Admin</h1>
+                </PageHeader>
+            </Section>
+
+            {/* Loading Message */}
+            {loading && <Loading progressMessage={loadingMessage}/>}
+
+
+            {/* Error Message */}
+            {error ?
+
+                <Section autoPadding>
+                    <Error error={error}/>
+                </Section>
+
+                :
+                <>
+                    {!session?
+                        loginSection()
+                        :
+                        adminPage()
+                    }
+                </>
+            }
+
+
+
+
+            {/* Footer */}
+            <Section>
+                <Footer/>
+            </Section>
+        </>
+    )
+}
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,4 +1,4 @@
-//set PATH=%PATH%;C:\Users\max.tyson\Downloads\node-v18.18.2-win-x64\node-v18.18.2-win-x64
+//set PATH=%PATH%;C:\Users\max.tyson\Downloads\node-v21.6.1-win-x64
 
 import React, {useEffect, useRef} from "react";
 
