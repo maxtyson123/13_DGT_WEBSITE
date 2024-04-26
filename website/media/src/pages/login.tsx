bc492893ece3f52801c3a8811cc91e380c24b51e
Max Tyson <98maxt98@gmail.com>
2024-04-26 15:32:33 +1200

    Work On Login Display

--- website/media/src/pages/login.tsx
+++ website/media/src/pages/login.tsx
@@ -1,16 +1,25 @@
+import styles from '@/styles/pages/login.module.css'
 export default function Login(){
     return(
         <>
-            {/* Log In Circle Icon */}
-            <div>
+            <div className={styles.container}>
+                {/* Log In Circle Icon */}
+                <div className={styles.cicrcles}>
+                    <div className={styles.circle1}>
+                        <div className={styles.circle2}>
 
+                        </div>
+                    </div>
+                </div>
 
-            </div>
+                {/* Text */}
+                <h1> Lets Connect</h1>
+                <h1> Together</h1>
 
-            {/* Buttons */}
-            <div>
+                {/* Buttons */}
                 <button> Log In</button>
-                <button> Sign Up</button>
+                <button className={styles.signup}> Sign Up</button>
+
             </div>
         </>
     )
